c3ca9102b5193cefed17e359ba4fcbe25316464a
gandul-jago <ghaniyagst@gmail.com>
2026-06-10 12:54:04 +0700

    update

--- lib/ui/menu/pages/table_resto_form.dart
+++ lib/ui/menu/pages/table_resto_form.dart
@@ -10,7 +10,10 @@ class PostTableResto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return block provider
-    return TableRestoForm();
+    return BlocProvider(
+      create: (context) => SubjectBloc(),
+      child: TableRestoForm(),
+    );
   }
 }
 
@@ -31,7 +34,7 @@ class _TableRestoFormState extends State<TableRestoForm> {
   @override
   void initState() {
     super.initState();
-    createTableRestoBloc 
+    createTableRestoBloc
   }
 
   @override
@@ -48,101 +51,101 @@ class _TableRestoFormState extends State<TableRestoForm> {
       appBar: AppBar(title: Text('Table Resto Form')),
       body: BlocBuilder<CreateTableRestoBloc, CreateTableRestoState>(
         builder: (context, state) {
-          if(state is CreateTableRestoError)
-          return Form(
-            key: formKey,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  spacing: 20,
-                  children: [
-                    TextFormField(
-                      controller: tecCode,
-                      keyboardType: TextInputType.name,
-                      validator: (value) =>
-                      value == null || value.isEmpty
-                          ? 'Kode meja masih kosong'
-                          : null,
-                      decoration: InputDecoration(
-                        labelText: 'Kode Meja',
-                        hintText: 'Masukkan kode meja',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
+          if (state is CreateTableRestoError)
+            return Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: 20,
+                    children: [
+                      TextFormField(
+                        controller: tecCode,
+                        keyboardType: TextInputType.name,
+                        validator: (value) =>
+                        value == null || value.isEmpty
+                            ? 'Kode meja masih kosong'
+                            : null,
+                        decoration: InputDecoration(
+                          labelText: 'Kode Meja',
+                          hintText: 'Masukkan kode meja',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
                         ),
                       ),
-                    ),
-                    TextFormField(
-                      controller: tecName,
-                      keyboardType: TextInputType.name,
-                      validator: (value) =>
-                      value == null || value.isEmpty
-                          ? 'Nama meja masih kosong'
-                          : null,
-                      decoration: InputDecoration(
-                        labelText: 'Nama Meja',
-                        hintText: 'Masukkan nama meja',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                      TextFormField(
+                        controller: tecName,
+                        keyboardType: TextInputType.name,
+                        validator: (value) =>
+                        value == null || value.isEmpty
+                            ? 'Nama meja masih kosong'
+                            : null,
+                        decoration: InputDecoration(
+                          labelText: 'Nama Meja',
+                          hintText: 'Masukkan nama meja',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
                         ),
                       ),
-                    ),
-                    TextFormField(
-                      controller: tecCapacity,
-                      keyboardType: TextInputType.number,
-                      validator: (value) =>
-                      value == null || value.isEmpty
-                          ? 'Kapasitas meja masih kosong'
-                          : null,
-                      decoration: InputDecoration(
-                        labelText: 'Kapasitas Meja',
-                        hintText: 'Masukkan kapasitas meja',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                      TextFormField(
+                        controller: tecCapacity,
+                        keyboardType: TextInputType.number,
+                        validator: (value) =>
+                        value == null || value.isEmpty
+                            ? 'Kapasitas meja masih kosong'
+                            : null,
+                        decoration: InputDecoration(
+                          labelText: 'Kapasitas Meja',
+                          hintText: 'Masukkan kapasitas meja',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 60,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme
-                              .of(
-                            context,
-                          )
-                              .colorScheme
-                              .inversePrimary,
-                          foregroundColor: Theme
-                              .of(context)
-                              .colorScheme
-                              .secondary,
-                        ),
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            final tableRestoParam = TableRestoParam(
-                             tecCode.text,
-                             tecName.text,
-                             int.parse(tecCapacity.text.toString()),
-                            );
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                  content: Text('Data berhasil disimpan...')),
-                            );
-                          }
-                        },
-                        child: Text(
-                          'SIMPAN',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
+                      Container(
+                        width: double.infinity,
+                        height: 60,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Theme
+                                .of(
+                              context,
+                            )
+                                .colorScheme
+                                .inversePrimary,
+                            foregroundColor: Theme
+                                .of(context)
+                                .colorScheme
+                                .secondary,
+                          ),
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              final tableRestoParam = TableRestoParam(
+                                tecCode.text,
+                                tecName.text,
+                                int.parse(tecCapacity.text.toString()),
+                              );
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text('Data berhasil disimpan...')),
+                              );
+                            }
+                          },
+                          child: Text(
+                            'SIMPAN',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
+            );
         },
       ),
     );
